387bd42542e991d67894f3d2ab854986f6821bdb
daymanhoffmann <hofffmannn@gmail.com>
2018-09-15 11:58:45 +0200

    fix the error control

--- myscript/script.py
+++ myscript/script.py
@@ -4,13 +4,19 @@ import requests
 arguments = sys.argv
 
 if len(arguments) > 2:
-	print("\n Bad use. Pleas insert only 1 parameters. For cities with complex name, write with next separator: _ \n")
+	print("\n\tBad use. Please insert only 1 parameters. For cities with complex name, write with next separator: _ \n")
+
 else:
 	name_city = arguments[1].replace("_", " ") if "_" in arguments[1] else arguments[1]
-	query_web = requests.get('http://0.0.0.0/dayman_weather/' + name_city)
-	
-	result = query_web.json()
-	temp = int(result["temperature"])
-	city = result["city"]
+	try:
+		query_web = requests.get('http://0.0.0.0/dayman_weather/' + name_city)
+		result = query_web.json()
+		
+		temp = int(result["temperature"])
+		city = result["city"]
 
-	print("\n Weather forecast for tomorrow in {} Temperature: {} ºC \n".format(city, temp))
+		print("\n\tWeather forecast for tomorrow in {} Temperature: {} ºC \n".format(city, temp))
+
+	except:
+		print('\n\tInsert a correct city\n')
+		
